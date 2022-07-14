4765a4a5615ffe0b1a79382ad945d1518fb961db
D4Q2 <61206792+D4Q2@users.noreply.github.com>
2022-07-14 11:11:10 -0400

    Update brute-hash.py

--- PasswordCracker/brute-hash.py
+++ PasswordCracker/brute-hash.py
@@ -3,43 +3,35 @@ import string
 import sys
 import hashlib
 
-# The algorithms that hashlib supports
-acceptedAlgos = ["sha1","sha224","sha256","sha384","sha512"]
-
 # The function to guess the password by brute force
 def guess(algo, password, number):
 	chars = string.ascii_lowercase + string.ascii_uppercase + string.digits
 	attempts = 0
-	for password_length in range(1,9):
+	for password_length in range(1,int(number)+1):
 		for guess in itertools.product(chars, repeat=password_length):
 			attempts += 1
 			raw_guess = ''.join(guess)
-			h = hashlib.new(algo)
-			h.update(password.encode('utf-8'))
+			h = hashlib.new("sha256")
+			h.update(raw_guess.encode('utf-8'))
 			guess = h.hexdigest()
 			if guess == password:
-				return 'Original Password is {} found in {} guesses.'.format(raw_guess, attempts)
+				return 'Original Password is \'{}\' found in {} guesses.'.format(raw_guess, attempts)
 			else:
-				print('Guess #{} is {} hashed as {}.'.format(attempts, raw_guess, guess))
+				print('Guess #{} is {} hashed as {}.'.format(attempts, raw_guess, h.hexdigest()))
 
-# If the user entered enough arguments
-if len(sys.argv) >= 4:
-	# If the user entered a supported hash algorithm
-	if sys.argv[1] in acceptedAlgos:
-		# python3 bh.py algo hash len -- Run the guesser
-		print(guess(sys.argv[1].lower(), sys.argv[2], sys.argv[3]))
-	# If the user didn't enter a supported hash algorithm
-	else:
-		# Print the supported hash algorithms
-		print('Hash algorithm not supported. Enter one of these: ')
-		for algo in acceptedAlgos:
-			print(algo)
-# If the user didn't enter enough arguments
-else:
-	algoList = "Supported Hash Algorithms: "
-	for algo in acceptedAlgos:
-		print(algo.upper())
-	algorithm = input("Hash Algorithm: ")
-	hashToCrack = input("Hash to Crack: ")
-	maxPasswordLength = input("Max Password Length: ")
-	print(guess(algorithm, hashToCrack, maxPasswordLength))
+# Print out the supported hash algorithms for this platform
+algoList = "Supported Hash Algorithms:"
+for algo in hashlib.algorithms_available:
+	algoList += "  " + algo.upper()
+print (algoList)
+
+# Keep asking until the user enters a supported hash algorithm
+algorithm = ""
+while not algorithm.lower() in hashlib.algorithms_available:
+    algorithm = input("Enter a supported algorithm:")
+    
+# Get the hash to crack and the max length to stop at
+hashToCrack = input("Hash to Crack: ")
+maxPasswordLength = input("Max Password Length: ")
+# Run the guesser
+print(guess(algorithm.lower(), hashToCrack, maxPasswordLength))
