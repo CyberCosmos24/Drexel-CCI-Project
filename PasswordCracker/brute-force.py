686d6b550dab72f2fa3af1a314d46ac671605031
D4Q2 <61206792+D4Q2@users.noreply.github.com>
2022-07-14 11:18:49 -0400

    Update brute-force.py

--- PasswordCracker/brute-force.py
+++ PasswordCracker/brute-force.py
@@ -1,20 +1,14 @@
 import itertools
 import string
-import sys
 
-def guess(password):
-	chars = string.ascii_lowercase + string.digits
+def guess(passLength):
+	chars = string.ascii_letters + string.digits + string.punctuation
 	attempts = 0
-	for password_length in range(1,9):
+	for password_length in range(1,int(passLength)+1):
 		for guess in itertools.product(chars, repeat=password_length):
 			attempts += 1
 			guess = ''.join(guess)
-			if guess == password:
-				return 'Password is {} found in {} guesses.'.format(guess, attempts)
-			else:
-				print('{} guesses'.format(attempts))
-n = len(sys.argv)
-if n > 1:
-	print(guess(sys.argv[1]))
-else:
-	print('Enter the password to crack as an argument.')
+			print(guess)
+length = input("Max Password Length: ")
+print("Sending all passwords up to {} characters".format(length)) 
+print(guess(length))
