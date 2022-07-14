4b92e0e9a4e0ab398a1eb19ff35e52b9cb88d1f2
Cosmos <ajaynonu24@gmail.com>
2022-07-14 12:34:44 -0400

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 echo "1 - PortScanner"
 echo "2 - IP-Lookup"
-echo "2 - Brute-Force"
-echo "2 - Brute-Hash"
-echo "2 - IP-Lookup"
+echo "3 - Brute-Force"
+echo "4 - Brute-Hash"
+echo "5 - HTTP-Requests"
 echo "Enter the tool you want to use: "
+read tool;
+case $tool in
+  1)
+    python3 PortScanner/pscan.py
+    ;;
 
-read user
+  2)
+    python3 IP-Info/lookup.py
+    ;;
 
-case user in 
-    1) python3 PortScanner/pscan.py;;
-    2) python3 IP-Info/lookup.py;;
-    2) python3 PasswordCracker/brute-force.py;;
-    2) python3 PasswordCracker/brute-hash.py;;
+  3)
+    python3 PasswordCracker/brute-force.py
+    ;;
+
+  4)
+    python3 PasswordCracker/brute-hash.py
+    ;;
+
+  5)
+    python3 HTTP-Requests/request.py
+    ;;
+
+  *)
+    ;;
 esac
