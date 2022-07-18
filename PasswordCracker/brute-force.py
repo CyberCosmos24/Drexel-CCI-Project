1b059d8c33b93ca9ffac9a90850e3b41809d608e
D4Q2 <61206792+D4Q2@users.noreply.github.com>
2022-07-18 10:39:32 -0400

    Add Min Length to Brute Pass Cracker

--- PasswordCracker/brute-force.py
+++ PasswordCracker/brute-force.py
@@ -1,14 +1,38 @@
-import itertools
-import string
+import itertools # For efficient looping
+import string # For string manipulation and characters
 
-def guess(passLength):
+# Run through all of the guesses from the min length to the max length
+def guess(minPassLength, maxPassLength):
+	# All of the characters
 	chars = string.ascii_letters + string.digits + string.punctuation
+	# Counter to track how many guesses we've made
 	attempts = 0
-	for password_length in range(1,int(passLength)+1):
+	# Loop through the possible lengths
+	for password_length in range(minPassLength,maxPassLength+1):
+		# Loop through all of the guess options
 		for guess in itertools.product(chars, repeat=password_length):
+			# increase the attempts counter
 			attempts += 1
+			# Create the guess
 			guess = ''.join(guess)
+			# Print out the guess
 			print(guess)
-length = input("Max Password Length: ")
-print("Sending all passwords up to {} characters".format(length)) 
-guess(length)
+
+### Get the min length and make sure it's a number
+ml = "" # The string the user inputs
+# While the input isn't a number or is less than 1, ask again
+while not ml.is_numeric() or int(ml) < 1:
+	ml = input("Min Password Length: ")
+min_length = int(ml) # The number we get from the user input
+
+# Get the max length and make sure it's a number
+l = "" # The string the user inputs
+# While the input isn't a number or is less than the min length, ask again
+while not length.is_numeric or int(length) < min_length:
+	length = input("Max Password Length: ")
+length = 0 # The number we get from the user input
+
+# Let the user know what we're doing
+print("Sending all passwords from {} characters to {} characters".format(min_length, length)) 
+# Go through all of the guesses
+guess(min_length, length)
