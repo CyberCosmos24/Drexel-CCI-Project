cbae0fcdcb81a777c7b43ef76808d4928deadd6b
D4Q2 <61206792+D4Q2@users.noreply.github.com>
2022-07-20 16:04:49 -0400

    Update windows.sh

--- windows.sh
+++ windows.sh
@@ -2,9 +2,11 @@
 echo "1 - PortScanner"
 echo "2 - IP-Lookup"
 echo "3 - Geolocation-IP"
-echo "4 - Brute-Force"
-echo "5 - Brute-Hash"
-echo "6 - HTTP-Requests"
+echo "4 - Brute Password Cracker"
+echo "5 - Brute Hash Cracker"
+echo "6 - Wordlist Password Cracker"
+echo "7 - Wordlist Hash Cracker"
+echo "8 - HTTP-Requests"
 echo "Enter the tool you want to use: "
 read tool;
 case $tool in
@@ -21,14 +23,22 @@ case $tool in
     ;;
 
   4)
-    python3 PasswordCracker/brute-force.py
+    python3 PasswordCracker/brute-password.py
     ;;
 
   5)
     python3 PasswordCracker/brute-hash.py
     ;;
-    
+   
   6)
+    python3 PasswordCracker/wordlist-password.py
+    ;;
+
+  7)
+    python3 PasswordCracker/wordlist-hash.py
+    ;;
+
+  8)
     python3 HTTP-Requests/request.py
     ;;
 
